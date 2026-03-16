6259dc75cb1d4f104b976f19c51dc19fd04f05ec
algoquemeimporta-ux <algoquemeimporta@gmail.com>
2026-03-16 17:36:26 -0500

    Create FavoritosActivity.java

---
+++ FavoritosActivity.java
@@ -0,0 +1,14 @@
+package com.recetas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FavoritosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoritos);
+    }
+}
